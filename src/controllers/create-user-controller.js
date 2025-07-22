ca1818fd7c7548ccbdcf383242dc203ef782d9c9
Leonardo <ep.leonardorocha@gmail.com>
2025-07-22 18:59:02 -0300

    feat: ensure EmailAlreadyInUseError is used

--- src/controllers/create-user-controller.js
+++ src/controllers/create-user-controller.js
@@ -1,6 +1,7 @@
 import { CreateUserUseCase } from '../use-cases/create-user-use-case.js';
 import validator from 'validator';
 import { badRequest, created, serverError } from './helpers.js';
+import { EmailAlreadyInUseError } from '../errors/user.js';
 
 export class CreateUserController {
     async execute(httpRequest) {
@@ -44,6 +45,9 @@ export class CreateUserController {
             // retornar a resposta ao usuario (status code 201)
             return created(createdUser);
         } catch (error) {
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: error.message });
+            }
             console.error(error);
             return serverError({
                 message: 'Internal Server error.',
