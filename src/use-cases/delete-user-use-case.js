a3f04598c1bf4d2408ae229b25d46aaeb5502bed
Leonardo <ep.leonardorocha@gmail.com>
2025-07-28 19:06:27 -0300

    feat: add DeleteUserUseCase

---
+++ src/use-cases/delete-user-use-case.js
@@ -0,0 +1,11 @@
+import { PostgresDeleteUserRepository } from '../repositories/postgres/delete-user-repository';
+
+export class DeleteUserUseCase {
+    async execute(userId) {
+        const postgresDeletedUserRepository =
+            new PostgresDeleteUserRepository();
+        const deletedUser = await postgresDeletedUserRepository.execute(userId);
+
+        return deletedUser;
+    }
+}
