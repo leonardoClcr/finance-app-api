c51ed26ad83d95eb6f6a20a97cdf15a0ddf24273
Leonardo <ep.leonardorocha@gmail.com>
2025-07-28 19:14:21 -0300

    feat: add DeleteUserController

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 export * from './create-user-controller.js';
 export * from './get-user-by-id-controller.js';
 export * from './update-user-controller.js';
+export * from './delete-user-controller.js';
